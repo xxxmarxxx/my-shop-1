2969e35398e6c310be89a7908ab4b7be7f515bbf
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-15 15:13:45 +0100

    add title props and wrapper with class link to home

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-const PageHero = () => {
-  return <h4>page hero</h4>
+const PageHero = ({title}) => {
+  return <Wrapper className="section-center">
+    <h3>
+      <Link to="/">Home</Link>/ {title}
+      </h3>
+      </Wrapper>
 }
 
 const Wrapper = styled.section`
