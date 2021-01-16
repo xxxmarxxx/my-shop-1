5dc77449ed77204f30a71567cf5202712fc732a5
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-16 17:30:54 +0100

    poczatek nowej wersji

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,66 +2,20 @@ import React from "react";
 import styled from "styled-components";
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 
+    // pierwsza wersja 5 razy skopiowane i powiekszona liczba o 1
+    // <span>
+    // {stars >= 1 ? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf />  : 
+    // <BsStar /> }
+    // </span>
+
+
 const Stars = ({ stars, reviews }) => {
   // console.log(stars, reviews);
   return (
     <Wrapper>
       <div className="stars">
-        {/* star */}
-        <span>
-          {stars >= 1 ? (
-            <BsStarFill />
-          ) : stars >= 0.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
-        {/* star */}
-        <span>
-          {stars >= 2 ? (
-            <BsStarFill />
-          ) : stars >= 1.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
-        {/* star */}
-        <span>
-          {stars >= 3 ? (
-            <BsStarFill />
-          ) : stars >= 2.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
-        {/* star */}
-        <span>
-          {stars >= 4 ? (
-            <BsStarFill />
-          ) : stars >= 3.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
-        {/* star */}
-        <span>
-          {stars === 5 ? (
-            <BsStarFill />
-          ) : stars >= 4.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of star */}
+ 
+        
       </div>
       <p className="reviews">({reviews} customer reviews)</p>
     </Wrapper>
