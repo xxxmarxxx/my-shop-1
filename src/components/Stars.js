b8e3935eec284f5eb417212c25e30cd87768f9c9
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-16 18:19:39 +0100

    add new vesion starts

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,21 +2,33 @@ import React from "react";
 import styled from "styled-components";
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 
-    // pierwsza wersja 5 razy skopiowane i powiekszona liczba o 1
-    // <span>
-    // {stars >= 1 ? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf />  : 
-    // <BsStar /> }
-    // </span>
-
+// pierwsza wersja 5 razy skopiowane i powiekszona liczba o 1
+// <span>
+// {stars >= 1 ? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf />  :
+// <BsStar /> }
+// </span>
 
 const Stars = ({ stars, reviews }) => {
   // console.log(stars, reviews);
+
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5;
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <BsStarFill />
+        ) : stars >= number ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    );
+  });
+
   return (
     <Wrapper>
-      <div className="stars">
- 
-        
-      </div>
+      <div className="stars">{tempStars}</div>
       <p className="reviews">({reviews} customer reviews)</p>
     </Wrapper>
   );
