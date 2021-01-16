2d5dc14ec25ec150cde4237a44f9e8c04ad6848b
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-16 15:29:57 +0100

    add Int.NumberFormat EUR

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+    return new Intl.NumberFormat('de-DE', {
+        style: 'currency',
+        currency: 'EUR',
+    }).format(number / 100)
+    
+}
 
 export const getUniqueValues = () => {}
