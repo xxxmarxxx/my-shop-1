3ab5243ef95a76fffcd8fbf1c2c9126aac3d2795
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-19 15:54:59 +0100

    add companies

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -54,6 +54,25 @@ const Filters = () => {
             </div>
           </div>
           {/* end of categories */}
+          {/* companies */}
+          <div className="form-control">
+            <h5>company</h5>
+            <select
+              name="company"
+              value={company}
+              onChange={updateFilters}
+              className="company"
+            >
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} value={c}>
+                    {c}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          {/* end companies */}
         </form>
       </div>
     </Wrapper>
