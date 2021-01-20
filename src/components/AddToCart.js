5399bbe0d02bc55459c907c2a27014889cdb7db5
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-20 01:18:28 +0100

    add const and link to /cart

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,6 +6,7 @@ import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext();
   const { id, stock, colors } = product;
   // console.log(colors);
   const [mainColor, SetMainColor] = useState(colors[0]);
@@ -25,13 +26,11 @@ const AddToCart = ({ product }) => {
     setAmount((oldAmount) => {
       let tempAmount = oldAmount - 1;
       if (tempAmount < 1) {
-        tempAmount = 1
+        tempAmount = 1;
       }
       return tempAmount;
     });
   };
-
-
 
   return (
     <Wrapper>
@@ -60,7 +59,7 @@ const AddToCart = ({ product }) => {
           increase={increase}
           decrease={decrease}
         />
-        <Link to="/cart" className="btn">
+        <Link to="/cart" className="btn" onClick={() => addToCart(id, mainColor, amount, product)}>
           add to cart
         </Link>
       </div>
