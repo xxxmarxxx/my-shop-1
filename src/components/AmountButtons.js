2beea427f42683c2c58a67d480e3916b500f06e9
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-20 15:08:24 +0100

    cleaning

--- src/components/AmountButtons.js
+++ src/components/AmountButtons.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import styled from 'styled-components'
-import { FaPlus, FaMinus } from 'react-icons/fa'
+import React from "react";
+import styled from "styled-components";
+import { FaPlus, FaMinus } from "react-icons/fa";
 
-const AmountButtons = ({increase,decrease,amount}) => {
-  return <Wrapper className="amount-btns"> 
-  <button type="button" className="amount-btn" onClick={decrease}>
-    <FaMinus/>
-  </button>
-  <h2 className="amount">{amount}</h2>
-  <button type="button" className="amount-btn" onClick={increase}>
-    <FaPlus/>
-  </button>
-  </Wrapper>
-}
+const AmountButtons = ({ increase, decrease, amount }) => {
+  return (
+    <Wrapper className="amount-btns">
+      <button type="button" className="amount-btn" onClick={decrease}>
+        <FaMinus />
+      </button>
+      <h2 className="amount">{amount}</h2>
+      <button type="button" className="amount-btn" onClick={increase}>
+        <FaPlus />
+      </button>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   display: grid;
@@ -37,6 +39,6 @@ const Wrapper = styled.div`
   h2 {
     margin-bottom: 0;
   }
-`
+`;
 
-export default AmountButtons
+export default AmountButtons;
