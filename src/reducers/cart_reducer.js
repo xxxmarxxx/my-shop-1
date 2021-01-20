1e0f16e09e8a561688e8932892313cf8f5f82e9c
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-20 15:11:51 +0100

    add removeCartItem and clearCart

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -1,6 +1,7 @@
 import {
   ADD_TO_CART,
   CLEAR_CART,
+  CLEAR_FILTERS,
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
@@ -37,7 +38,13 @@ const cart_reducer = (state, action) => {
       return { ...state, cart: [...state.cart, newItem] };
     }
   }
-  return state;
+  if (action.type === REMOVE_CART_ITEM) {
+    const tempCart = state.cart.filter((item) => item.id !== action.payload);
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
+  }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
