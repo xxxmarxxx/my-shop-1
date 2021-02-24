f8b632b92ae3dbe2e895d12e7fada0ab3a6bb8ca
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-24 13:40:21 +0100

    add onClick

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -19,11 +19,15 @@ const CartButtons = () => {
           <span className="cart-value">{total_items}</span>
         </span>
       </Link>
-      <button type="button" className="auth-btn">
+      <button type="button" className="auth-btn" onClick={loginWithRedirect}>
         Login <FaUserPlus />
       </button>
-      <button type="button" className="auth-btn">
-        Logout <FaUserMinus/>
+      <button
+        type="button"
+        className="auth-btn"
+        onClick={() => logout({ returnTo: window.location.origin })}
+      >
+        Logout <FaUserMinus />
       </button>
     </Wrapper>
   );
