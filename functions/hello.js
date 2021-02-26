b8515a7e97468f90cc74208fe9c2a29b3b086cd1
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-26 14:05:57 +0100

    add items

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,8 @@
 // domain/.netlify/functions/hello
-
+const items = [
+  {id: 1, name: 'john'},
+  {id: 2, name: 'susan'},
+]
 exports.handler = async function (event, context) {
   return {
     statusCode: 200,
